05b2decf20e2a25d25811b625a1f0761cc0d9014
Brandon Torchia <bmt35@pitt.edu>
2017-01-23 18:02:08 -0500

    adding main driver and main panel

---
+++ Hnefatafl.java
@@ -0,0 +1,8 @@
+public class Hnefatafl 
+{
+    public static void main(String[] args) 
+    {
+		MainFrame mf = new MainFrame(); //number is meaniningless
+    }
+    
+}
---
+++ MainPanel.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+
+
+public class MainPanel extends JPanel 
+{ //this wil be the game board
+
+    // Current configura
+    public MainPanel() 
+    {
+		super();
+	}	
+}
