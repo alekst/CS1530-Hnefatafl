de535e83e91fe4c86aa2deafc9c3fd9997cf7d9a
Aleks Tapinsh <aleks.tapinsh@gmail.com>
2017-01-31 16:45:19 -0500

    minor fix

--- src/main/java/ButtonPanel.java
+++ src/main/java/ButtonPanel.java
@@ -5,37 +5,35 @@ import javax.swing.*;
 
 public class ButtonPanel extends JPanel 
 {
-  
-  private SaveButton _save;
+	
+	private SaveButton _save;
 
-  private LoadButton _load;
-  
-  private NewGameButton _newGame;
+	private LoadButton _load;
 
-  private ConcedeButton _concede;
+	private NewGameButton _newGame;
+
+	private ConcedeButton _concede;
     
   /**
   * Constructor - add all of the buttons to
   * the ButtonPanel.
   */
-    
-  public ButtonPanel(Board m) 
-  {
+	  public ButtonPanel(Board m) 
+	{
 
-    // Send a reference to the Main Panel
-    // to all of the buttons.
-	
+		// Send a reference to the Main Panel
+		// to all of the buttons.
+
 	_save = new SaveButton(m);
 	_load = new LoadButton(m);
 	_newGame = new NewGameButton(m);
 	_concede = new ConcedeButton(m);
 	setLayout(new FlowLayout());
 
-	  // Add all of the buttons
-	  add(_save);
-	  add(_load);
-	  add(_newGame);
-	  add(_concede);
-  }
-    
+	// Add all of the buttons
+	add(_save);
+	add(_load);
+	add(_newGame);
+	add(_concede);
+	}
 }
