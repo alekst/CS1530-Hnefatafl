568684cc6f564f18de6de09bd44c71faf0e69f6a
Aleks Tapinsh <aleks.tapinsh@gmail.com>
2017-02-28 01:03:50 -0500

    adds Manager, Player, Game classes

---
+++ src/main/java/Player.java
@@ -0,0 +1,72 @@
+public class Player
+{
+	private Manager _manager;
+	private boolean _white;
+	private boolean _turn;
+	
+	public Player()
+	{
+		// an empty constructor
+	}
+	
+	public Player(Manager manager)
+	{
+		_manager = manager;
+	}
+	
+	public void setWhite()
+	{
+		_white = true;
+		_turn = false;
+		
+	}
+	
+	public void setBlack()
+	{
+		_white = false;
+		_turn = true;
+	}
+	
+	public void doneWithTurn()
+	{
+		_turn = false; 
+	}
+	
+	public void newTurn()
+	{
+		_turn = true; 
+	}
+	
+	public boolean myTurn()
+	{
+		return _turn;
+	}
+	
+	
+	public Coordinate[] getPieces()
+	{
+		if (_white)
+		{
+			return _manager.getWhites();
+		}
+		else
+			return _manager.getBlacks();
+	}
+		
+	
+	public boolean isWhite()
+	{
+		return _white;
+	}
+	
+	public boolean hasWon()
+	{
+		Coordinate king = _manager.getKing();
+		if (_manager.hasKingEscaped(king) && _manager.isKingSurrounded(king))
+		{
+			return true;
+		}
+		return false;
+	}
+	
+}
