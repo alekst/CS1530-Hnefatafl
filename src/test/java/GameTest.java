32795a64993b9b859e9ed8131ae33c60fbcd00fc
Audrey Ann Blakely <aub7@pitt.edu>
2017-04-15 16:33:30 -0400

    resolves merge conflicts from develop
    * pulls in conflicting commits from develop

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,7 +1,8 @@
 import java.lang.*;
-import javax.swing.*;
-import org.junit.Test;
+import java.util.*;
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
 
 /*
  *	This class holds all tests for the Game class. 
@@ -10,6 +11,14 @@ import static org.junit.Assert.*;
  */
 public class GameTest
 {
+  //Tests if queryManager returns a Manager object
+	@Test
+	public void testQueryInstanceOfManager()
+	{
+		Game g = new Game();
+		assertThat(g.queryManager(), instanceOf(Manager.class));
+	}
+  
 	/*
 	 *	This test tests that getTurn gets the player turn
 	 */
